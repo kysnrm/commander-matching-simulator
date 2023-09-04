b1cbaa0cf704a2c45f89ffc8d5e73c545910dde2
kysnrm <kysn.rm@gmail.com>
2023-09-04 17:41:02 +0900

    シャッフルアルゴリズムを改善

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import { css } from "../styled-system/css";
 import exampleCommanders from "./assets/example";
 
 const shuffleArray = <T,>(array: T[]) => {
-  return array.slice().sort(() => Math.random() - Math.random());
+  const output: T[] = Array.from(array);
+
+  for (let i = output.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    const tmp = output[i];
+    output[i] = output[random];
+    output[random] = tmp;
+  }
+
+  return output;
 };
 
 function App() {
